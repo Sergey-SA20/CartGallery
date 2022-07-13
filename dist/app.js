5a3b9b15114bc6d9099aea6dde39f40f69757f6a
Sergey-SA20 <time2sgb@gmail.com>
2022-07-13 19:20:53 +0300

    first commit

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(active = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+slides[active].classList.add('active');
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        
+        slide.classList.add('active')
+    });
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide)=> {
+        slide.classList.remove('active')
+    });
+}
+}
+
+slidesPlugin(2);
